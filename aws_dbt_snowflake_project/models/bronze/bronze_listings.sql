4ec491f8c635da30364529d32142546298353a0c
Arsha Khan <khanarsha333@gmail.com>
2026-01-18 00:00:34 +0000

    Add files via upload

---
+++ aws_dbt_snowflake_project/models/bronze/bronze_listings.sql
@@ -0,0 +1,8 @@
+{% set incremental_flag = 1 %}
+{% set incremental_col = 'CREATED_AT' %}
+
+SELECT * FROM {{ source('staging', 'listings') }}
+
+{% if incremental_flag == 1 %}
+    where {{ incremental_col }} > (SELECT COALESCE(MAX({{ incremental_col }}), '1900-01-01') FROM {{this}})
+{% endif %}
